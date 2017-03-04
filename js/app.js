4152bc3f2ece395f60370e38557bdb1f2f9e9369
Patrcik <pertriquek171@gmail.com>
2017-03-04 11:21:40 -0500

    adding profile page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+
+
+
+$(".form-text").hide();
+
+$(".sign-in").onclick(function () {
+   alert("By signing in, you accept the terms and conditions");
+    $(".form-text").show();
+});
+
+
+
+
 var app = angular.module('front-app', []);
